007306b3da21bc77daed494d2207438bcc05a7a9
Karim Elein Castillo Ordinola <72017769+Mirak30@users.noreply.github.com>
2021-01-29 17:39:21 +0100

    Chat modificado

--- JER/marauder (Servidor)/src/main/java/es/sidelab/marauder/chatHandler.java
+++ JER/marauder (Servidor)/src/main/java/es/sidelab/marauder/chatHandler.java
@@ -9,30 +9,48 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+
 public class chatHandler extends TextWebSocketHandler{
 	private Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+	private ObjectMapper mapper = new ObjectMapper();
 	
-	
+	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		System.out.println("New session: " + session.getId());
+		System.out.println("New user: " + session.getId());
 		sessions.put(session.getId(), session);
 	}
 	
-	
+	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		System.out.println("Session closed: " + session.getId());
 		sessions.remove(session.getId());
 	}
 	
-	
+	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		sendOtherParticipants(session, message.getPayload());
+		
+		System.out.println("Message received: " + message.getPayload());
+		JsonNode node = mapper.readTree(message.getPayload());
+		
+		sendOtherParticipants(session, node);
 	}
 
-	private void sendOtherParticipants(WebSocketSession session, String payload) throws IOException {
+	private void sendOtherParticipants(WebSocketSession session, JsonNode node) throws IOException {
+
+		System.out.println("Message sent: " + node.toString());
+		
+		ObjectNode newNode = mapper.createObjectNode();
+		newNode.put("name", node.get("name").asText());
+		newNode.put("message", node.get("message").asText());
+		
+		
 		for(WebSocketSession participant : sessions.values()) {
 			if(!participant.getId().equals(session.getId())) {
-				participant.sendMessage(new TextMessage(payload));
+				participant.sendMessage(new TextMessage(newNode.toString()));
 			}
 		}
 	}
