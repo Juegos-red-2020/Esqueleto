d324af8a3349a90036873383d3dbd8f1d55cad38
M-Galvez <72497923+M-Galvez@users.noreply.github.com>
2021-02-01 17:40:17 +0100

    Versión completa de Marauder

--- JER/marauder (Servidor)/src/main/java/es/sidelab/marauder/App.java
+++ JER/marauder (Servidor)/src/main/java/es/sidelab/marauder/App.java
@@ -9,10 +9,11 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+
 @SpringBootApplication
 @EnableWebSocket
-public class App 
-{
+public class App implements WebSocketConfigurer{
+	
     public static void main( String[] args )
     {
         SpringApplication.run(App.class,args);
@@ -22,11 +23,54 @@ public class App
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 		registry.addHandler(createChatHandler(), "/chat")
 			.setAllowedOrigins("*");
+		registry.addHandler(createPositionHandler(), "/position")
+		.setAllowedOrigins("*");
+		registry.addHandler(createPlayerSelectHandler(), "/playerSelection")
+		.setAllowedOrigins("*");
+		registry.addHandler(createExactDevaPosXHandler(), "/exactDevaPositionX")
+		.setAllowedOrigins("*");
+		registry.addHandler(createExactDevaPosYHandler(), "/exactDevaPositionY")
+		.setAllowedOrigins("*");
+		registry.addHandler(createExactReniPosXHandler(), "/exactReniPositionX")
+		.setAllowedOrigins("*");
+		registry.addHandler(createExactReniPosYHandler(), "/exactReniPositionY")
+		.setAllowedOrigins("*");
+		registry.addHandler(createPlayerOnlineHandler(), "/online")
+		.setAllowedOrigins("*");
 	}
 	
 	@Bean
 	public chatHandler createChatHandler() {
 		return new chatHandler();
 	}
-}
+	
+	@Bean
+	public PositionHandler createPositionHandler() {
+		return new PositionHandler();
+	}
+	@Bean
+	public PlayerSelectHandler createPlayerSelectHandler() {
+		return new PlayerSelectHandler();
+	}
+	@Bean
+	public ExactDevaPosXHandler createExactDevaPosXHandler() {
+		return new ExactDevaPosXHandler();
+	}
+	@Bean
+	public ExactDevaPosYHandler createExactReniPosYHandler() {
+		return new ExactDevaPosYHandler();
+	}
+	
+	@Bean
+	public ExactReniPosXHandler createExactReniPosXHandler() {
+		return new ExactReniPosXHandler();
+	}
+	@Bean
+	public ExactReniPosYHandler createExactDevaPosYHandler() {
+		return new ExactReniPosYHandler();
+	}
+	@Bean
+	public PlayerOnlineHandler createPlayerOnlineHandler() {
+		return new PlayerOnlineHandler();
+	}
 }
--- JER/marauder (Servidor)/src/main/java/es/sidelab/marauder/ExactDevaPosXHandler.java
+++ JER/marauder (Servidor)/src/main/java/es/sidelab/marauder/ExactDevaPosXHandler.java
@@ -9,48 +9,30 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-
-public class chatHandler extends TextWebSocketHandler{
+public class ExactDevaPosXHandler extends TextWebSocketHandler{
 	private Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
-	private ObjectMapper mapper = new ObjectMapper();
 	
-	@Override
+	
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		System.out.println("New user: " + session.getId());
+		System.out.println("New session: " + session.getId());
 		sessions.put(session.getId(), session);
 	}
 	
-	@Override
+	
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		System.out.println("Session closed: " + session.getId());
 		sessions.remove(session.getId());
 	}
 	
-	@Override
+	
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		
-		System.out.println("Message received: " + message.getPayload());
-		JsonNode node = mapper.readTree(message.getPayload());
-		
-		sendOtherParticipants(session, node);
+		sendOtherParticipants(session, message.getPayload());
 	}
 
-	private void sendOtherParticipants(WebSocketSession session, JsonNode node) throws IOException {
-
-		System.out.println("Message sent: " + node.toString());
-		
-		ObjectNode newNode = mapper.createObjectNode();
-		newNode.put("name", node.get("name").asText());
-		newNode.put("message", node.get("message").asText());
-		
-		
+	private void sendOtherParticipants(WebSocketSession session, String payload) throws IOException {
 		for(WebSocketSession participant : sessions.values()) {
 			if(!participant.getId().equals(session.getId())) {
-				participant.sendMessage(new TextMessage(newNode.toString()));
+				participant.sendMessage(new TextMessage(payload));
 			}
 		}
 	}
