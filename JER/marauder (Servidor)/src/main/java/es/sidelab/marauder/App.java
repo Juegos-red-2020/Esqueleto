63b3ee9bae3b9dc6b24d675a014d04df05c51fa2
Karim Elein Castillo Ordinola <72017769+Mirak30@users.noreply.github.com>
2021-02-01 16:38:01 +0100

    Modificación del APP para el chat

--- JER/marauder (Servidor)/src/main/java/es/sidelab/marauder/App.java
+++ JER/marauder (Servidor)/src/main/java/es/sidelab/marauder/App.java
@@ -3,12 +3,30 @@ package es.sidelab.marauder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
+@EnableWebSocket
 public class App 
 {
     public static void main( String[] args )
     {
         SpringApplication.run(App.class,args);
     }
+    
+    @Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(createChatHandler(), "/chat")
+			.setAllowedOrigins("*");
+	}
+	
+	@Bean
+	public chatHandler createChatHandler() {
+		return new chatHandler();
+	}
+}
 }
